3c1f66b30caa031e0596bb3cf9899ee31697def4
Rafael Fernández <rfernandez@ficodes.com>
2023-11-14 10:35:40 +0100

    ADDS more examples

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,28 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Blinker', 'IBM\\ Plex\\ Mono', 'Menlo', 'monospace'],
+      body: ['Blinker', 'IBM\\ Plex\\ Mono', 'Menlo', 'monospace'],
+    },
+    extend: {
+      colors: {
+        primary: {
+          50: '#00add3',
+          100: '#2d58a7',
+        },
+        secondary: {
+          50: '#dde6f6',
+          100: '#14274a',
+        },
+      },
+    }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
