e1a813a7b086df1a35a0c6dc8183606eb2a831b4
Lara Miñones Rodríguez <lminones@ficodes.com>
2024-12-12 15:11:28 +0100

    FIX contact us form

--- src/app/pages/contact-us/contact-us-form.component.ts
+++ src/app/pages/contact-us/contact-us-form.component.ts
@@ -9,15 +9,32 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 export class ContactUsFormComponent implements OnInit {
   contactForm = new FormGroup({
-    email: new FormControl('', [Validators.required]),
-    name: new FormControl(''),
-    lastname: new FormControl(''),
-    organization: new FormControl(''),
-    role: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+    name: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+    lastname: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+    organization: new FormControl('', [Validators.maxLength(100)]),
+    role: new FormControl('', [Validators.maxLength(100)]),
     message: new FormControl('', [Validators.required]),
   });
   
   ngOnInit() {
+  }
+
+  sendMail() {
+    let message = 'First name: '+this.contactForm.value.name+ '%0A' +
+    'Last name: '+this.contactForm.value.lastname+ '%0A';
+    if(this.contactForm.value.organization!=''){
+      message=message+'Organization: '+this.contactForm.value.organization+ '%0A';
+    }
+    if(this.contactForm.value.role!=''){
+      message=message+'Role: '+this.contactForm.value.role+ '%0A';
+    }
+    message=message+this.contactForm.value.message;
+
+    const mailtoLink = `mailto:info@dome-marketplace.eu?body=${message}`;
+    
+    // Open the mailto link
+    window.location.href = mailtoLink;
   }
 
 }
