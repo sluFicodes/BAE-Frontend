1b74779dbaf2a668db77bc3dcb148736cdc51dd7
Lara Miñones Rodríguez <lminones@ficodes.com>
2025-02-13 16:33:37 +0100

    ADD close button to details modal + FIX header card's style + ADD register 3 links dropdown + ADD change landing offerings style

--- src/app/offerings/featured/featured.component.ts
+++ src/app/offerings/featured/featured.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {EventMessageService} from "../../services/event-message.service";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {Category} from "../../models/interfaces";
+import {LocalStorageService} from "../../services/local-storage.service";
 
 @Component({
   selector: 'bae-off-featured',
@@ -7,5 +11,31 @@ import { CommonModule } from '@angular/common';
   styleUrl: './featured.component.css'
 })
 export class FeaturedComponent {
-
+  constructor(
+    private eventMessage: EventMessageService,
+    private localStorage: LocalStorageService,
+    private router: Router
+    ) {
+    }
+  searchByCategory(){
+    //MOCKED CATEGORY
+    //TO-DO CHANGE THIS TO MATCH ALL THE CATEGORIES ON THE LIST
+    const cat = {
+      "id": "urn:ngsi-ld:category:b6d7450f-3da7-4a90-9655-32e6d342ee4a",
+      "href": "urn:ngsi-ld:category:b6d7450f-3da7-4a90-9655-32e6d342ee4a",
+      "description": "",
+      "isRoot": true,
+      "lastUpdate": "2024-11-04T08:26:58.683281084Z",
+      "lifecycleStatus": "Launched",
+      "name": "Software as a Service (SaaS)",
+      "validFor": {
+          "startDateTime": "2024-11-04T08:26:53.558Z"
+      },
+      "children": []
+    } as Category;
+    this.localStorage.addCategoryFilter(cat);
+    this.eventMessage.emitAddedFilter(cat);
+    this.router.navigate(['/search']);
+    
+  }
 }
