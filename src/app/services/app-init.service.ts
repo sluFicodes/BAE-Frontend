36df66e0984a95ae2e3e20f421baedfb63415bf6
Lara Miñones Rodríguez <lminones@ficodes.com>
2025-04-28 09:02:00 +0200

    ADD ticket 831716

--- src/app/services/app-init.service.ts
+++ src/app/services/app-init.service.ts
@@ -23,7 +23,7 @@ export class AppInitService {
                 environment.SEARCH_ENABLED = config.searchEnabled;
                 environment.DOME_TRUST_LINK = config.domeTrust;
                 environment.DOME_ABOUT_LINK = config.domeAbout;
-                environment.PURCHASE_ENABLED = true;
+                environment.PURCHASE_ENABLED = config.purchaseEnabled;
                 environment.DOME_REGISTER_LINK = config.domeRegister;
                 environment.DOME_PUBLISH_LINK = config.domePublish;
                 environment.KB_ONBOARDING_GUIDELINES_URL = config.domeOnboardingGuidelines;
