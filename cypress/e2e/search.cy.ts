770446a6981c9eda0e3a0fa77410ec6861e6e0b9
Shengxing Lu <slu@ficodes.com>
2025-05-29 11:13:33 +0200

    catalog form test

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,11 @@
+import { checkHeaderPostLogin, checkHeaderPreLogin } from "./dashboard.cy"
+
+describe('/search',{
+    viewportHeight: 800,
+    viewportWidth: 1080,
+  },
+  () => {
+    it('', ()=>{
+        
+    })
+  })
