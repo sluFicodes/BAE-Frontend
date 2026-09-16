6888295a64108afe4b2030a2ce0d6e5d7bc461a6
Francisco de la Vega <fdelavega@ficodes.com>
2026-09-16 10:59:59 +0200

    Recover search bar in catalog details

--- src/app/services/pagination.service.spec.ts
+++ src/app/services/pagination.service.spec.ts
@@ -3,16 +3,35 @@ import { TranslateModule } from '@ngx-translate/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PaginationService } from './pagination.service';
+import { ApiServiceService } from './product-service.service';
 
 describe('PaginationService', () => {
   let service: PaginationService;
+  let api: ApiServiceService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({ imports: [HttpClientTestingModule, RouterTestingModule, TranslateModule.forRoot()] });
     service = TestBed.inject(PaginationService);
+    api = TestBed.inject(ApiServiceService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should pass catalog keywords to unfiltered and category-filtered catalog product requests', async () => {
+    const getByCatalogSpy = spyOn(api, 'getProductsByCatalog').and.resolveTo([]);
+    const getByCategoryAndCatalogSpy = spyOn(api, 'getProductsByCategoryAndCatalog').and.resolveTo([]);
+
+    await service.getProductsByCatalog(0, 'edge', [], 'catalog-1');
+    expect(getByCatalogSpy).toHaveBeenCalledWith('catalog-1', 0, 'edge');
+
+    await service.getProductsByCatalog(0, 'edge', [{ id: 'category-1', name: 'IaaS' }], 'catalog-1');
+    expect(getByCategoryAndCatalogSpy).toHaveBeenCalledWith(
+      [{ id: 'category-1', name: 'IaaS' }],
+      'catalog-1',
+      0,
+      'edge'
+    );
   });
 });
